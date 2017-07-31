4427ebd4deb1c18a6c3d2dcf199b22d5602fdd71
Pablo Panero <pablo.panero@cern.ch>
2017-07-31 15:41:48 +0200

    update doc + topic_backlog script

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Randscripts
 Random scripts/small programs needed for some task
 
-## compare_topics.py 
+## topic_compare.py 
 
 Enables to check the progress of a kafka topic using ConsumerGroupCommand. This is needed because GetOffsetShell does not support SSL brokers.
 
 Example call:
 
 ```bash
-python compare_topics.py --kafka-home /path/to/kafka_2.11-0.10.2.0 --bootstrap-server  some-kafka-broker.ch:9093 --command-config consumer.properties --group kafka-console-group --topics topic1 topic2
+python topic_compare.py --kafka-home /path/to/kafka_2.11-0.10.2.0 --bootstrap-server  some-kafka-broker.ch:9093 --command-config consumer.properties --group kafka-console-group --topics topic1 topic2
 ```
 
 Example output:
@@ -27,3 +27,27 @@ You killed the monitoring so I'll kill your consumers!
 Killing consumer with PID 27177
 Killing consumer with PID 27178
 ```
+
+## topic_backlog.py 
+
+Enables to check the backlog by taking the timestamp of the consumed message of a kafka topic using kafka-consumer.sh.
+
+Example call:
+
+```bash
+python topic_backlog.p --kafka-home /path/to/kafka_2.11-0.10.2.0 --bootstrap-server  some-kafka-broker.ch:9093 --command-config consumer.properties --topics topic1 topic2 topic3 -n 1
+```
+
+Note that the consumer.config must NOT have any consumer group configured so the new consumer created are not stucked in old consumer queues with old timestamps.
+
+Example output:
+
+```bash
+Checking backlog for topics ['topic1', 'topic2', 'topci3']
+---------------------Timestamp diff----------------------
+|    topic1             |   2017-07-31T15:31:23Z        |
+|    topic2             |   2017-07-31T15:31:17Z        |
+|    topci3             |   2017-07-31T12:04:57Z        |
+---------------------------------------------------------
+```
+
---
+++ topic_backlog.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import argparse
+import subprocess
+import time
+import os
+import json
+from datetime import datetime
+
+
+# Argument parsing
+parser = argparse.ArgumentParser(description='Monitor Kafka - Compare topics processing')
+
+parser.add_argument('--kafka-home',
+                     help='Directory where kafka bin folder is placed')
+parser.add_argument('--bootstrap-server',
+                     help='Kafka bootstrap server including port (e.j. kafka-broker.something.ch:9093)')
+parser.add_argument('--command-config',
+                     help='Property file containing configs to be passed to Admin client and Consumer. Please note that if there is a consumer group the timestamp will not be accurate')
+parser.add_argument('--topics',  metavar='topic', nargs='+',
+                     help='Topic to check')
+parser.add_argument('-n',  metavar='nmesg',
+                     help='Number of messages to get the timestamp from')
+
+args = parser.parse_args()
+	
+print("Checking backlog for topics {0}".format(args.topics))
+
+# Need to open the consumer for each topic. Please note that in general the fact of opening consumers 
+# (and also taking the measure) sequentially might result in some small error in the topic's progress
+# kafka.tools.GetOffsetShell does not support brokers over SSL. Therefore we need to opt for 
+# consumerGroupCommand which needs consumers
+
+# Opening consumers
+
+FNULL = open(os.devnull, 'w') # To avoid the stdout of the process to be displayed
+
+print("---------------------Timestamp diff----------------------")
+for topic in args.topics:
+	consumed_value = subprocess.check_output(
+		[
+		"{0}{1}".format(args.kafka_home, "/bin/kafka-console-consumer.sh"),
+		"--bootstrap-server", 
+		args.bootstrap_server,
+		"--consumer.config",
+		"{0}/config/{1}".format(args.kafka_home, args.command_config),
+		"--topic",
+		topic,
+		"--max-messages",
+		str(args.n),
+		"--new-consumer"
+		],
+		stderr=FNULL)
+	dtts = datetime.fromtimestamp(json.loads(consumed_value)['timestamp']/1000)
+	ts = dtts.strftime("%Y-%m-%dT%H:%M:%SZ")
+	print("|\t {0} \t|\t{1}\t|".format(topic, ts))
+	
+print("---------------------------------------------------------")
